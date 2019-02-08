e8634d18fa5ef018f76a3acb66b4a83955ebc8b3
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-08 04:46:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       @status = 'complete'
     elsif sender.balance < amount
       @status = 'rejected'
-      "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
     end
   end
 
