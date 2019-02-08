eadd71e736a6942ffca7b5f7fb904df33b521823
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-08 04:11:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
-  attr_accessor :status
+  attr_accessor :status, :sender, :reciever
 
-  def initialize
+  def initialize(status, sender, reciever)
     @status = 'pending'
   end
 
