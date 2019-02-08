685cad172e00f8e962d9c52e3932e2cfbeac1797
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-08 04:40:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if status != 'complete' && valid? == true
+    if status == 'pending' && valid?
       sender.balance -= amount
       receiver.balance += amount
       @status = 'complete'
