feb8a3044b7fdf2b5c7e52caf98ad254b3ad5754
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-08 03:49:26 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,23 @@
 class BankAccount
+  attr_accessor :amount
 
+  def initialize(amount)
+    @amount
+  end
+
+  def deposit
+
+  end
+
+  def display_balance
+
+  end
+
+  def valid?
+
+  end
+
+  def close_account
+
+  end
 end
