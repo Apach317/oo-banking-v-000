9314dd5c37e641ea6f2139cb5be8bba310a8eb0b
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-08 03:51:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
 class Transfer
-  # your code here
+  attr_accessor :transfer, :sender, :reciever, :amount
+
+  def initialize(transfer = "pending")
+    @transfer = transfer
+    @sender = sender
+    @reciever = reciever
+    @amount
+
+  end
+
+  def valid?
+
+  end
+
+  def execute_transaction
+
+  end
+
+  def reverse_transer
+
+  end
 end
